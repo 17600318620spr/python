3a34950f3b14edfd7aa8ba1210195b6649ec3589
pr.shi <52943539+17600318620spr@users.noreply.github.com>
2020-04-01 22:08:52 +0800

    计算两个矩阵的 Hadamard 乘积
    添加程序注释

--- matrixmul.py
+++ matrixmul.py
@@ -1,3 +1,6 @@
+'''
+计算两个矩阵的 Hadamard 乘积。要求输入矩阵的行/列数（在这里假设我们使用的是 n × n 的矩阵）。
+'''
 #!/usr/bin/env python3
 n = int(input("Enter the value of n: "))
 print("Enter values for the Matrix A")
@@ -18,3 +21,10 @@ for x in c:
         print(str(y).rjust(5), end=' ')
     print()
 print("-" * 7 * n)
+
+'''
+这里我们使用了几次列表推导式。
+[int(x) for x in input().split()] 首先通过 input() 获得用户输入的字符串，
+再使用 split() 分割字符串得到一系列的数字字符串，然后用 int() 从每个数字字符串创建对应的整数值。
+我们也使用了 [a[i][j] * b[i][j] for j in range(n)] 来得到矩阵乘积的每一行数据。
+'''
